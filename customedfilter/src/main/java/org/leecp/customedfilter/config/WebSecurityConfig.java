518d5a8f548ad6c6c0f74b01068d8aee45c6b05d
Luke <leejanezeroone10@gmail.com>
2019-08-15 18:57:14 +0800

    更新 readme.md

--- customedfilter/src/main/java/org/leecp/customedfilter/config/WebSecurityConfig.java
+++ customedfilter/src/main/java/org/leecp/customedfilter/config/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package org.leecp.customedfilter.config;
 
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
