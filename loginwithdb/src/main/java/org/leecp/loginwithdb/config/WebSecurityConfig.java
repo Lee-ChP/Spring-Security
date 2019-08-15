fd0f92924b83f4277e2150b6d87396129ffd973e
Luke <leejanezeroone10@gmail.com>
2019-08-15 16:13:40 +0800

    添加密码加密的功能

--- loginwithdb/src/main/java/org/leecp/loginwithdb/config/WebSecurityConfig.java
+++ loginwithdb/src/main/java/org/leecp/loginwithdb/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -39,12 +40,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
      * 将自定义获取的用户信息打包给authenticationManager去对比校验，
      */
     protected void configure(AuthenticationManagerBuilder builder) throws Exception{
-        builder.userDetailsService(this.userDetailsService);
+        builder.userDetailsService(this.userDetailsService).passwordEncoder(encoder());
     }
 
     @Bean
+    /**
+     * 密码加密
+     */
     public static PasswordEncoder encoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
 }
--- loginwithdb/src/main/java/org/leecp/loginwithdb/service/serviceImpl/UserServiceImpl.java
+++ loginwithdb/src/main/java/org/leecp/loginwithdb/service/serviceImpl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import org.leecp.loginwithdb.dto.User;
 import org.leecp.loginwithdb.repository.UserRepository;
 import org.leecp.loginwithdb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,6 +19,12 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void saveUser(User user) {
+        /**
+         * 加密
+         */
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String password = user.getPassword();
+        user.setPassword(encoder.encode(password));
         userRepository.save(user);
     }
 }
