84c444860d15a62a21835524ea02fcebba5d7331
Luke <leejanezeroone10@gmail.com>
2019-08-16 11:59:37 +0800

    为loginwithdb添加动态权限管理功能

--- loginwithdb/src/main/java/org/leecp/loginwithdb/config/WebSecurityConfig.java
+++ loginwithdb/src/main/java/org/leecp/loginwithdb/config/WebSecurityConfig.java
@@ -3,15 +3,15 @@ package org.leecp.loginwithdb.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SuppressWarnings("deprecation")
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true) //开启动态权限管理
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
--- loginwithdb/src/main/java/org/leecp/loginwithdb/web/UserController.java
+++ loginwithdb/src/main/java/org/leecp/loginwithdb/web/UserController.java
@@ -1,8 +1,17 @@
 package org.leecp.loginwithdb.web;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
@@ -12,6 +21,28 @@ public class UserController {
     public String user( Model model){
         model.addAttribute("success", "LoginSuccessfully");
         return "user/user";
+    }
+
+    @GetMapping("/user/update")
+    public String updateAuth() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        List<GrantedAuthority> newAuth = new ArrayList<>(auth.getAuthorities());
+        newAuth.add(new SimpleGrantedAuthority("ROLE_Vip"));
+        Authentication newAuthentication = new UsernamePasswordAuthenticationToken(auth.getPrincipal(), auth.getCredentials(),newAuth);
+        SecurityContextHolder.getContext().setAuthentication(newAuthentication);
+        return "updateAuth";
+    }
+
+
+    /**
+     * ROLE_前缀是必须的
+     * @return
+     */
+    @GetMapping("/vip")
+    @Secured("ROLE_Vip")
+    public String vip(){
+        return "vip";
     }
 
 }
